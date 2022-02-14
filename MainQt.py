c8eae2ec878eaf3e0124645ac9af5fa2261998f4
Doraz1 <62758770+Doraz1@users.noreply.github.com>
2022-02-14 23:59:48 +0200

    Add files via upload

--- MainQt.py
+++ MainQt.py
@@ -12,28 +12,30 @@ import pyautogui
 import pygame.mixer
 from mutagen.mp3 import MP3
 import numpy as np
-from Scripts.myCustomWidgets import AutoClicker, ChangeUserList, AutoClickerAnimation
+from Scripts.myCustomWidgets import AutoClicker, ChangeUserList
 from Scripts.database import PlayerDataBase
-from Scripts.threads import ProgressBarThread, ACBlinkThreads, ResetGuiThread, ACMoveThreads
-from Scripts.threads import Ros2QTSubscriber, QT2RosLEDPublisher, QT2RosNavPublisher, NavMasterNode
+from Scripts.threads import ProgressBarThread, ACBlinkThread
 from Scripts.screens import MainScreen, InstructionsScreen, SecondScreen, GameScreen, ScoreScreen
+
 # region init
 # load assets and songs
 ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
 SONGS_PATH = os.path.join(ROOT_PATH, "Songs")
 ASSET_PATH = os.path.join(ROOT_PATH, "Assets")
+
+
 # endregion
 
 
 class MyWindow(QMainWindow):
-    def __init__(self, width, height, title):
+    def __init__(self, width, height, title, DEBUG):
         super().__init__()
         full_screen_size = pyautogui.size()
-        self.simActive = False # simulate camera locations vs. use true locations
-        # self.simActive = True
+        # self.simActive = False # simulate camera locations vs. use true locations
+        self.simActive = True
         self.width = width
         self.height = height
-        self.setGeometry(int((full_screen_size[0] - width)/2), int((full_screen_size[1] - height)/2), width, height)
+        self.setGeometry(int((full_screen_size[0] - width) / 2), int((full_screen_size[1] - height) / 2), width, height)
 
         # Database
         self.playerDataBase = PlayerDataBase()
@@ -64,12 +66,15 @@ class MyWindow(QMainWindow):
         self.total_song_length = -1
         self.bpm = -1
         self.song_path = ""
+        self.note_times = None
+        self.notes = []
 
         # game parameters
         self.running = False
         self.first_run = True
         self.paused = False
-
+        self.DEBUG = DEBUG
+        self.number_of_robots = 1
         self.activate_screen("main")  # Load the main screen on game startup
         self.show()  # render
 
@@ -122,19 +127,24 @@ class MyWindow(QMainWindow):
         self.activate_screen("score")
         pygame.mixer.music.fadeout(2000)
 
-        self.playerDataBase.update_score(self.song_name, self.score) # attempt to update based on currently active player
-        self.resetGui()
+        self.playerDataBase.update_score(self.song_name,
+                                         self.score)  # attempt to update based on currently active player
+        self.reset()
 
     def btn_choose_song(self):
         self.song_name = "song2"
 
         self.song_path = os.path.join(SONGS_PATH, "One Kiss_cropped.mp3")
         self.audio = MP3(self.song_path)
-        self.bpm = 123.85  # using audacity we got 123, updated a bit
+        self.bpm = 123.85  # One kiss BPM - using audacity we got 123, updated a bit
 
+        self.get_autoclicker_notes('one kiss')
+        if self.first_run:
+            self.first_run = False
+            self.initialize_game_widgets()
 
-        notes = self.get_autoclicker_notes('one kiss')
-        self.begin_game(notes)
+        self.start_game()
+
     # endregion
 
     def get_autoclicker_notes(self, song_name):
@@ -149,139 +159,88 @@ class MyWindow(QMainWindow):
         if song_name == 'one kiss':
             # iterations to play on, assuming each iteration is the shortest beat
             # up to 430 iterations
-            easyUp = ACBlinkThreads.easyUpLevel
-            ac1_notes = [easyUp * iter for iter in iters if iter<(iters[-1]//easyUp) + 1]  # notes only till song end
-            ac2_notes = [easyUp * iter for iter in iters if iter<(iters[-1]//easyUp) + 1]
-            ac3_notes = [easyUp * iter for iter in iters if iter<(iters[-1]//easyUp) + 1]
+            easyUp = ACBlinkThread.easyUpLevel
+            ac1_notes = [easyUp * iter for iter in iters if
+                         iter < (iters[-1] // easyUp) + 1]  # notes only till song end
+            ac2_notes = [easyUp * iter for iter in iters if iter < (iters[-1] // easyUp) + 1]
+            ac3_notes = [easyUp * iter for iter in iters if iter < (iters[-1] // easyUp) + 1]
             notes.append(ac1_notes)
             notes.append(ac2_notes)
             notes.append(ac3_notes)
-        return notes
+        self.notes = notes
 
-    def begin_game(self, notes):
-        try:
-            if self.first_run:
-                '''
-                Create the threads and the autoclickers for the first time
-                '''
-                # self.first_run = False
-                self.progressBarThread = ProgressBarThread(self, pygame.mixer.music)
-                self.progressBarThread.progress.connect(self.updateProgressBar)
+    def initialize_game_widgets(self):
+        '''
+        Create the threads and the autoclickers for the first time
+        '''
+        self.progressBarThread = ProgressBarThread(self, pygame.mixer.music)
+        self.progressBarThread.progress.connect(self.update_progress_bar)
 
-                # Create AutoClickers and their animations (the animations MUST belong to the win for some reason)
-                self.screens["game"].autoClickers = []
-                self.screens["game"].autoClickers.append(AutoClicker(self, 0).animation)
-                self.screens["game"].autoClickers.append(AutoClicker(self, 1).animation)
-                self.screens["game"].autoClickers.append(AutoClicker(self, 2).animation)
+        # Create AutoClickers and their animations (the animations MUST belong to the win for some reason)
+        if not self.simActive:
+            rclpy.init()
 
-                # Create AutoClicker simulators
-                self.clickerMoveThread = ACMoveThreads(self)
-                self.clickerMoveThread.clicker_pos.connect(self.clickerMoveThread.moveAutoClickers)
+        self.screens["game"].autoclickers = []
 
-                if self.simActive:
-                    print("Simulators are active!")
-                    self.clickerBlinkThread = ACBlinkThreads(self, notes)
-                    self.clickerMoveThread.move_clickers_to_initial_positions(self)
-                else:
-                    # use real camera locations and LED commands
-                    print("Real life data!")
-                    rclpy.init()
-                    'Clicker 1 location subscriber'
-                    self.rosSubscriberThread = Ros2QTSubscriber(self)
-                    self.rosSubscriberThread.camera_msg.connect(self.clickerMoveThread.move_based_on_inputs)
+        clicker_1 = AutoClicker(self, 0)
+        self.screens["game"].autoclickers.append(clicker_1)
+        self.screens["game"].autoClickerAnims.append(clicker_1.animation)
+        if self.number_of_robots >= 2:
+            clicker_2 = AutoClicker(self, 1)
+            self.screens["game"].autoclickers.append(clicker_2)
+            self.screens["game"].autoClickerAnims.append(clicker_2.animation)
+        if self.number_of_robots >= 3:
+            clicker_3 = AutoClicker(self, 2)
+            self.screens["game"].autoclickers.append(clicker_3)
+            self.screens["game"].autoClickerAnims.append(clicker_3.animation)
 
-                    'LED publisher'
-                    self.rosLEDPublisherThread = QT2RosLEDPublisher()
-                    self.clickerBlinkThread = ACBlinkThreads(self, notes, self.rosLEDPublisherThread)
-                    self.clickerBlinkThread.thread1.LED_msg.connect(self.rosLEDPublisherThread.run_led_publisher)
-                    # self.clickerBlinkThread.thread2.LED_msg.connect(self.rosPublisherThread.run_led_publisher)
-                    # self.clickerBlinkThread.thread3.LED_msg.connect(self.rosPublisherThread.run_led_publisher)
-
-                    'Navigation publisher'
-                    self.rosNavPublisherThread = QT2RosNavPublisher()
-                    # self.clickerBlinkThread.thread1.LED_msg.connect(self.rosLEDPublisherThread.run_nav_publisher)
-
-                    msg = "Please place the AutoClickers in their initial positions."
-                    # button_reply = QMessageBox.question(self, 'PyQt5 message', msg,
-                    #                                     QMessageBox.Close | QMessageBox.Cancel, QMessageBox.Cancel)
-                    #
-                    # if button_reply != QMessageBox.Close:
-                    #     return
-
-                self.resetGuiThread = ResetGuiThread(self)
-
-            self.activate_screen("game")
-
-            self.running = True
-
-            pygame.mixer.init()
-            pygame.mixer.music.load(self.song_path)
-            pygame.mixer.music.play()
-
-            self.progressBarThread.start()
-            self.clickerBlinkThread.start()
-            if self.simActive:
-                self.clickerMoveThread.start()
-            else:
-                self.rosSubscriberThread.start()
-
-
-        except Exception as e:
-            print(e)
-            exit(-2)
-    def printNum(self, x, y, theta):
-        # self.printThisTime += 1
-        # if self.printThisTime % 10 == 0:
-        #     print(f"Robot coordinates received! ({x}, {y}) with angle {theta}")
-        print(f"Robot coordinates received! ({x}, {y}) with angle {theta}")
 
     # region thread events
-    def updateProgressBar(self, progress):
+
+    def start_game(self):
+        self.running = True
+        for clicker in self.screens["game"].autoclickers:
+            clicker.start_threads()
+
+        if self.simActive:
+            print("Simulators are active!")
+        else:
+            print("Real life data!")
+
+            msg = "Please place the AutoClickers in their initial positions."
+            # button_reply = QMessageBox.question(self, 'PyQt5 message', msg,
+            #                                     QMessageBox.Close | QMessageBox.Cancel, QMessageBox.Cancel)
+            #
+            # if button_reply != QMessageBox.Close:
+            #     return
+        self.activate_screen("game")
+
+        pygame.mixer.init()
+        pygame.mixer.music.load(self.song_path)
+        pygame.mixer.music.play()
+
+        self.progressBarThread.start()
+
+    def update_progress_bar(self, progress):
         self.screens['game'].progressBars[0].setValue(progress)
 
         self.score = 0
-        for clickerAnim in self.screens["game"].autoClickers:
-            self.score += 15 * clickerAnim.parent.score
+        for clicker in self.screens["game"].autoclickers:
+            self.score += 15 * clicker.score
 
         self.screens['game'].show()
 
-    # def moveAutoClickers(self, x, y, index):
-    #     self.screens['game'].autoClickers[index].move(x, y)
-
-    def resetGui(self):
+    def reset(self):
         self.running = False
         self.score = 0
-
-        self.killAllThreads()
-        for clickerAnim in self.screens["game"].autoClickers:
-            clickerAnim.resetBlink()
-            clickerAnim.parent.score = 0
-
-    def killAllThreads(self):
         self.progressBarThread.stop()
-        self.clickerBlinkThread.stop()
-        self.clickerMoveThread.stop()
-        self.clickerBlinkThread.stop()
-        if self.simActive:
-            pass
-        else:
-            self.rosSubscriberThread.stop()
-            self.rosLEDPublisherThread.stop()
-            self.rosNavPublisherThread.stop()
+        for clicker in self.screens["game"].autoclickers:
+            clicker.reset()
+
+        if not self.simActive:
             rclpy.shutdown()
 
-
-
-
-
-
-
-
-
-
-
-
-    #endregion
+    # endregion
 
 
 def launch_game():
@@ -294,13 +253,11 @@ def launch_game():
         width, height = 1600, 900
 
     try:
-        MyWindow(width, height, "Clicker Hero")
+        MyWindow(width, height, "Clicker Hero", DEBUG=True)
         sys.exit(app.exec_())
     except Exception as e:
-            print(e)
-            exit(1)
-
-
+        print(e)
+        exit(1)
 
 
 if __name__ == "__main__":
